61cc15e65a06da24098bcbdb6e522c0419bf7b76
janet <janet@LAPTOP-U2VFUDF8>
2021-02-04 22:13:43 -0600

    2.0

--- src/Test/HolaMundo.java
+++ src/Test/HolaMundo.java
@@ -12,7 +12,7 @@ package Test;
 public class HolaMundo {
     
     public static void main(String[] args) {
-        System.out.println("Bienvenidos a la clase de POO");
+        System.out.println("Bienvenidos a la clase de POO GRUPO LSC3A");
     
     }
     
